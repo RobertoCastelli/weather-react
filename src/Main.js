23e6bf65cdc77e117cd8d869a050277619cbbb64
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-12-17 23:51:56 +0100

    still buggy

--- src/Main.js
+++ src/Main.js
@@ -1,51 +1,56 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect} from 'react'
 
 function Main() {
 
     const api = 'http://api.openweathermap.org/data/2.5/weather?q='
     const key = '95e94eae366bd6d895f5424258602103'
-    const [city, setCity] = useState('Bordighera')
     const [temperature, setTemperature] = useState('')
     const [description, setDescription] = useState('')
     const [latitude, setLatitude] = useState('')
     const [longitude, setLongitude] = useState('')
     const [icon, setIcon] = useState('')
-   
-    const handleChange = e => {    
-        e.preventDefault()
-        setCity(e.target.value)
+    const [city, setCity] = useState('Leeds')
+
+
+
+    const handleChange = e => {
+        console.log(city) 
     }
 
-    const handleSubmit = (e) => {
-        console.log('ciao')
-    }
-    
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(city) 
+    } 
 
     useEffect(() => {
-        async function fetchData() {
-            const response = await fetch(`${api}${city}&appid=${key}`)
+            const fetchData = async (cityName) => {
+            const response = await fetch(`${api}${cityName}&appid=${key}`)
             const data = await response.json()
             const iconImage = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             console.log(data)
+            setCity(data.name)
             setIcon(iconImage)
-            setTemperature(data.main.temp)
+            setTemperature(Math.floor(data.main.temp - 273,15))
             setDescription(data.weather[0].description)
             setLongitude(data.coord.lon)
             setLatitude(data.coord.lat)
         }
-            fetchData()
-        }, [])
+            fetchData(city)
+        }, []) 
 
     return (
-        <div onSubmit={handleSubmit} className="container">
-            <div className="console">
-                <input onChange={handleChange} type="text" placeholder=" search city for weather info"/>
-                <button >SEARCH</button>
-            </div>
+        <div className="container">
+            <form onSubmit={handleSubmit} className="console" >
+                <input 
+                    onChange={handleChange} 
+                    type="text" 
+                    placeholder=" search city for weather info"/>
+                <button>SEARCH</button>
+            </form>
             <div className="info">
                 <h2>{city}</h2>
                 <img src={icon} alt="meteoImage" />
-                <h2>{(temperature-273,15)}&deg;</h2>
+                <h2>{temperature}&deg;</h2>
                 <p>{description}</p>
                 <p>lat {latitude} | lon {longitude}</p>
             </div>
